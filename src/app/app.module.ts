1f84a4b43d2c1cd8c6e3276f513fbe50741819fb
Vinokurov Igor <vin-ig@yandex.ru>
2025-09-01 20:09:13 +0300

    Footer Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
@@ -18,6 +20,7 @@ import { HeaderComponent } from './components/header/header.component';
   bootstrap: [
       AppComponent,
       HeaderComponent,
+      FooterComponent,
   ]
 })
 export class AppModule { }
